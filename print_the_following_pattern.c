2c040c0589b4db5da8015012f0f834559746b551
DEEPAKPOLLIPALLI <100121495+DEEPAKPOLLIPALLI@users.noreply.github.com>
2022-07-03 10:10:31 +0530

    #include<stdio.h> int main() {     int n,i,k;     scanf("%d",&n);     char j;     for(i=0;i<n;i++)     {         for(j=65+i;j<k+n;j++)         {             k=65+i;             printf("%c ",k);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,25 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int a;
-    scanf("%d",&a);
-    int i,j;
-    for(i=0;i<a;i++)
+    int n,i,k;
+    scanf("%d",&n);
+    char j;
+    for(i=0;i<n;i++)
     {
-        for(j=0;j<a-(i+1);j++)
+        for(j=65+i;j<k+n;j++)
         {
-            printf(" ");
-        }
-        for(j=i;j>0;j--)
-        {
-            printf("%d",j);
-        }
-        for(j=0;j<i+1;j++)
-        {
-            printf("%d",j);
+            k=65+i;
+            printf("%c ",k);
         }
         printf("
 ");
     }
-    return 0;
 }
